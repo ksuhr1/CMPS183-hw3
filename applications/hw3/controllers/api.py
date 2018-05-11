f6bcf8590c2db93b10d5bacff965e15eea00a3ed
ksuhr1 <ksuhr@ucsc.edu>
2018-05-11 00:43:16 -0700

    Gott add button and memo to pop up

--- applications/hw3/controllers/api.py
+++ applications/hw3/controllers/api.py
@@ -2,26 +2,74 @@
 
 import random
 
+#
+# def get_memos():
+#     start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
+#     end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
+#     memos = []
+#     has_more = False
+#     rows = db().select(db.memo.ALL, limitby=(start_idx, end_idx + 1))
+#     for i, r in enumerate(rows):
+#         if i < end_idx - start_idx:
+#             t = dict(
+#                 id=r.id,
+#                 title=r.title,
+#                 content=r.memo_content,
+#             )
+#             memos.append(t)
+#         else:
+#             has_more = True
+#     logged_in = auth.user is not None
+#     return response.json(dict(
+#         memos=memos,
+#         logged_in=logged_in,
+#         has_more=has_more,
+#     ))
+
 
 def get_memos():
     start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
     end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
-
-    # generate a lot of data
-
     memos = []
-
-    for i in range(start_idx, end_idx):
-        t = dict(
-            title=random.choice(['Hi', 'WAS SUP', 'WHAT IS HAPPENING']),
-            text=random.choice(['this sucks', 'wow', 'wish i was home']),
-        )
-        memos.append(t)
-    has_more = True
-    logged_in = False
+    has_more = False
+    rows = db().select(db.memo.ALL, limitby=(start_idx, end_idx + 1))
+    for i, r in enumerate(rows):
+        if i < end_idx - start_idx:
+            t = dict(
+                id=r.id,
+                title=r.title
+            )
+            memos.append(t)
+        else:
+            has_more = True
+    logged_in = auth.user is not None
     return response.json(dict(
         memos=memos,
         logged_in=logged_in,
         has_more=has_more,
     ))
 
+
+@auth.requires_signature()
+def add_memo():
+    t_id = db.memo.insert(
+        title=request.vars.title
+    )
+    t = db.memo(t_id)
+    return response.json(dict(memo=t))
+
+
+@auth.requires_signature()
+def del_memo():
+    db(db.memo.id == request.vars.memo_id).delete()
+    return "ok"
+
+
+#@auth.requires_signature()
+# def add_memo():
+#     t_id = db.memo.insert(
+#         title=request.vars.title,
+#         memo_content=request.vars.content,
+#     )
+#     t = db.memo(t_id)
+#     return response.json(dict(memo=t))
--- applications/hw3/models/tables.py
+++ applications/hw3/models/tables.py
@@ -15,14 +15,14 @@ def get_user_email():
     return auth.user.email if auth.user is not None else None
 
 
-db.define_table('checklist',
+db.define_table('memo',
                 Field('user_email', default=get_user_email()),
                 Field('title'),
-                Field('memo', 'text'),
+                Field('memo_content', 'text'),
                 Field('updated_on', 'datetime', update=datetime.datetime.utcnow()),
                 Field('is_public', 'boolean', default=False)
                 )
 
 # after defining tables, uncomment below to enable auditing
-auth.enable_record_versioning(db)
+#auth.enable_record_versioning(db)
 
