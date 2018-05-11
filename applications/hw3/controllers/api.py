e302db945f8ef20ac0272c9a55d7e0632426081e
ksuhr1 <ksuhr@ucsc.edu>
2018-05-11 02:04:31 -0700

    added trash button working on edit button, there are some errors

--- applications/hw3/controllers/api.py
+++ applications/hw3/controllers/api.py
@@ -1,31 +1,5 @@
 # Here go your api methods.
 
-import random
-
-#
-# def get_memos():
-#     start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
-#     end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
-#     memos = []
-#     has_more = False
-#     rows = db().select(db.memo.ALL, limitby=(start_idx, end_idx + 1))
-#     for i, r in enumerate(rows):
-#         if i < end_idx - start_idx:
-#             t = dict(
-#                 id=r.id,
-#                 title=r.title,
-#                 content=r.memo_content,
-#             )
-#             memos.append(t)
-#         else:
-#             has_more = True
-#     logged_in = auth.user is not None
-#     return response.json(dict(
-#         memos=memos,
-#         logged_in=logged_in,
-#         has_more=has_more,
-#     ))
-
 
 def get_memos():
     start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
@@ -37,7 +11,8 @@ def get_memos():
         if i < end_idx - start_idx:
             t = dict(
                 id=r.id,
-                title=r.title
+                title=r.title,
+                memo_content=r.memo_content
             )
             memos.append(t)
         else:
@@ -52,24 +27,26 @@ def get_memos():
 
 @auth.requires_signature()
 def add_memo():
+
     t_id = db.memo.insert(
-        title=request.vars.title
+        title=request.vars.title,
+        memo_content=request.vars.memo_content
     )
+
     t = db.memo(t_id)
     return response.json(dict(memo=t))
+
+@auth.requires_signature()
+def edit_memo():
+    memo = db(db.memo.id == request.vars.id).select().first()
+    memo.update_record(title=request.vars.title, post_content=request.vars.memo_content)
+
+    print memo
+    return dict()
+
 
 
 @auth.requires_signature()
 def del_memo():
     db(db.memo.id == request.vars.memo_id).delete()
     return "ok"
-
-
-#@auth.requires_signature()
-# def add_memo():
-#     t_id = db.memo.insert(
-#         title=request.vars.title,
-#         memo_content=request.vars.content,
-#     )
-#     t = db.memo(t_id)
-#     return response.json(dict(memo=t))
--- applications/hw3/static/js/default_index.js
+++ applications/hw3/static/js/default_index.js
@@ -1,102 +1,3 @@
-// This is the js for the default/index.html view.
-//
-// var app = function() {
-//
-//     var self = {};
-//
-//     Vue.config.silent = false; // show all warnings
-//
-//     // Extends an array
-//     self.extend = function(a, b) {
-//         for (var i = 0; i < b.length; i++) {
-//             a.push(b[i]);
-//         }
-//     };
-//
-//      // Enumerates an array.
-//     var enumerate = function(v) { var k=0; return v.map(function(e) {e._idx = k++;});};
-//
-//     function get_memos_url(start_idx, end_idx){
-//         var pp = {
-//             start_idx: start_idx,
-//             end_idx: end_idx
-//         };
-//         //replace this
-//         return memos_url + "?" + $.param(pp);
-//     }
-//     self.get_memos = function() {
-//         $.getJSON(get_memos_url(0,10), function(data){
-//             self.vue.memos = data.memos;
-//             self.vue.has_more = data.has_more;
-//             self.vue.logged_in = data.logged_in;
-//             enumerate(self.vue.memos);
-//         })
-//     };
-//
-//     self.get_more = function() {
-//         var num_memos = self.vue.memos.length;
-//         $.getJSON(get_memos_url(num_memos, num_memos + 10), function(data){
-//             self.vue.has_more = data.has_more;
-//             self.extend(self.vue.memos, data.memos);
-//             enumerate(self.vue.memos);
-//         });
-//     };
-//
-//     self.add_memo_button = function() {
-//         //The button to add a memo has been pressed.
-//         if(self.vue.logged_in)
-//             self.vue.is_adding_memo = !self.vue.is_adding_memo;
-//     };
-//
-//     // self.add_memo = function () {
-//     //     $.post(add_memo_url,
-//     //         {
-//     //             title: self.vue.form_title,
-//     //             content: self.vue.form_content
-//     //         },
-//     //         function (data) {
-//     //             $.web2py.enableElement($("#add_memo_submit"));
-//     //             self.vue.memos.unshift(data.memo);
-//     //             console.log(self.vue.memos.length);
-//     //             enumerate(self.vue.memos);
-//     //             self.vue.is_adding_memo = !self.vue.is_adding_memo;
-//     //             self.vue.form_content="";
-//     //             self.vue.form_title="";
-//     //         });
-//     // };
-//
-//     // Complete as needed.
-//     self.vue = new Vue({
-//         el: "#vue-div",
-//         delimiters: ['${', '}'],
-//         unsafeDelimiters: ['!{', '}'],
-//         data: {
-//             is_adding_memo:false,
-//             memos:[],
-//             logged_in: false,
-//             has_more: false,
-//             form_content: null,
-//             form_title: null,
-//             show: true
-//         },
-//         methods: {
-//             get_more:self.get_more,
-//             add_memo_button: self.add_memo_button,
-//             add_memo: self.add_memo
-//         }
-//
-//     });
-//     self.get_memos();
-//     $("#vue-div").show();
-//
-//     return self;
-// };
-//
-// var APP = null;
-//
-// // This will make everything accessible from the js console;
-// // for instance, self.x above would be accessible as APP.x
-// jQuery(function(){APP = app();});
 
 var app = function() {
 
@@ -111,6 +12,9 @@ var app = function() {
         }
     };
 
+     // Enumerates an array.
+    var enumerate = function(v) { var k=0; return v.map(function(e) {e._idx = k++;});};
+
     function get_memos_url(start_idx, end_idx) {
         var pp = {
             start_idx: start_idx,
@@ -124,6 +28,7 @@ var app = function() {
             self.vue.memos = data.memos;
             self.vue.has_more = data.has_more;
             self.vue.logged_in = data.logged_in;
+            enumerate(self.vue.memos);
         })
     };
 
@@ -132,28 +37,64 @@ var app = function() {
         $.getJSON(get_memos_url(num_memos, num_memos + 10), function (data) {
             self.vue.has_more = data.has_more;
             self.extend(self.vue.memos, data.memos);
+            enumerate(self.vue.memos);
         });
     };
 
     self.add_memo_button = function () {
         // The button to add a memo has been pressed.
-        self.vue.is_adding_memo = !self.vue.is_adding_memo;
+        if(self.vue.logged_in)
+            self.vue.is_adding_memo = !self.vue.is_adding_memo;
     };
 
     self.add_memo = function () {
         // The submit button to add a memo has been added.
-        $.post(add_memo_url,
+        $.memo(add_memo_url,
             {
                 title: self.vue.form_title,
+                memo_content: self.vue.form_content
             },
             function (data) {
                 $.web2py.enableElement($("#add_memo_submit"));
                 self.vue.memos.unshift(data.memo);
+                console.log(self.vue.memos.length);
+
+                //if memos have length greater than 10, has_more is true
+                if(self.vue.memos.length > 10 ){
+                    self.vue.has_more = true;
+                }
+                self.vue.is_adding_memo = !self.vue.is_adding_memo;
+                self.vue.form_title="";
+                self.vue.form_content="";
             });
     };
 
+    self.edit_memo_submit = function () {
+        // The submit button to add a track has been added.
+        $.memo(edit_memo_url,
+            {
+                memo_content: self.vue.edit_content,
+                id: self.vue.edit_id
+            },
+            function (data) {
+                $.web2py.enableElement($("#edit_memo_submit"));
+                self.vue.editing = !self.vue.editing;
+            });
+    };
+    self.edit_memo = function(memo_id) {
+        console.log("yes");
+        self.vue.editing = !self.vue.editing;
+        self.vue.edit_id = memo_id;
+    };
+
+    self.cancel_edit = function () {
+        self.vue.editing = !self.vue.editing;
+        self.vue.edit_id = 0;
+
+    };
+
     self.delete_memo = function(memo_id) {
-        $.post(del_memo_url,
+        $.memo(del_memo_url,
             {
                 memo_id: memo_id
             },
@@ -184,7 +125,8 @@ var app = function() {
             memos: [],
             logged_in: false,
             has_more: false,
-            form_title: null
+            form_title: null,
+            form_content: null
 
         },
         methods: {
